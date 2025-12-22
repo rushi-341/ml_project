392f3e3c66c482a168209cdaaceaa8e633ccf993
Yalamanchili Rushi <rushiyalamanchili2@gmail.com>
2025-12-22 19:03:42 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -33,6 +33,10 @@ class DataIngestion:
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of data is completed")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
 
         except Exception as e:
             raise CustomException(e,sys)
